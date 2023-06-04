b076b010d1516710b510e2c9f12119a708a8e363
Herman Mark <athauf94@gmail.com>
2023-06-04 16:36:43 +0300

    HS-1 - Added clean project

---
+++ src/main/java/com/hcentralizer/app/HcentralizerApplication.java
@@ -0,0 +1,13 @@
+package com.hcentralizer.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HcentralizerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HcentralizerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hcentralizer/app/HcentralizerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hcentralizer.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HcentralizerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
